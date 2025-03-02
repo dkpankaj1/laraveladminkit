215d3ddf5f228f9e1f504d28daa3076bedaaaa99
Dipankar kumar pankaj <dipankarpankaj38@gmail.com>
2025-03-02 16:48:53 +0530

    fixes

--- src/Console/InstallAdminKit.php
+++ src/Console/InstallAdminKit.php
@@ -30,17 +30,23 @@ class InstallAdminKit extends Command
 
         $paths = [
             ['source' => __DIR__ . '/../../stubs/app/Http/Controllers', 'destination' => app_path('Http/Controllers')],
+            ['source' => __DIR__ . '/../../stubs/app/Http/Middleware', 'destination' => app_path('Http/Middleware')],
             ['source' => __DIR__ . '/../../stubs/app/Http/Requests', 'destination' => app_path('Http/Requests')],
             ['source' => __DIR__ . '/../../stubs/kadso/resource/views', 'destination' => resource_path('views')],
             ['source' => __DIR__ . '/../../stubs/app/View/Components', 'destination' => app_path('View/Components')],
             ['source' => __DIR__ . '/../../stubs/kadso/assets', 'destination' => public_path('assets')],
         ];
 
+        $this->info('💽 Installing AdminKit...');
+        $progressBar = $this->output->createProgressBar(count($paths) + 1);
+        $progressBar->start();
+
         foreach ($paths as $path) {
             if ($filesystem->exists($path['source'])) {
                 $filesystem->ensureDirectoryExists($path['destination']);
                 $filesystem->copyDirectory($path['source'], $path['destination']);
             }
+            $progressBar->advance();
         }
 
         // Copy Routes
@@ -51,6 +57,10 @@ class InstallAdminKit extends Command
             $filesystem->copy($routeSource, $routeDestination);
         }
 
-        $this->info('AdminKit installed successfully!');
+        $progressBar->advance();
+        $progressBar->finish();
+
+        $this->newLine();
+        $this->info('✅ AdminKit installed successfully!');
     }
 }
