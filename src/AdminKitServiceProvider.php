2820cee5dc33bab87f21b7453cf9f647a2198e59
Dipankar kumar pankaj <dipankarpankaj38@gmail.com>
2025-03-02 10:42:34 +0530

    initial commit

---
+++ src/AdminKitServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Dkpankaj1\AdminKit;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminKitServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
