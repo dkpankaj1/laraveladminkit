1ddabfd641273b2a6c8ec36610aa31d113482deb
Dipankar kumar pankaj <dipankarpankaj38@gmail.com>
2025-03-02 16:09:23 +0530

    fixes

--- src/AdminKitServiceProvider.php
+++ src/AdminKitServiceProvider.php
@@ -2,13 +2,16 @@
 
 namespace Dkpankaj1\AdminKit;
 
+use Dkpankaj1\AdminKit\Console\InstallAdminKit;
 use Illuminate\Support\ServiceProvider;
 
 class AdminKitServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        //
+        $this->commands([
+            InstallAdminKit::class,
+        ]);
     }
 
     public function boot()
