822c5a57aaf35e861b50f27b5a6a5f0b15230752
Dipankar kumar pankaj <dipankarpankaj38@gmail.com>
2025-05-28 00:04:09 +0530

    ixes

--- src/Console/InstallAdminKit.php
+++ src/Console/InstallAdminKit.php
@@ -42,6 +42,7 @@ class InstallAdminKit extends Command
             ['source' => __DIR__ . '/../../stubs/approx/app/Http/Middleware', 'destination' => app_path('Http/Middleware')],
             ['source' => __DIR__ . '/../../stubs/approx/app/Http/Requests', 'destination' => app_path('Http/Requests')],
             ['source' => __DIR__ . '/../../stubs/approx/app/View/Components', 'destination' => app_path('View/Components')],
+            ['source' => __DIR__ . '/../../stubs/approx/app/Support', 'destination' => app_path('Support')],
             ['source' => __DIR__ . '/../../stubs/approx/resource/views', 'destination' => resource_path('views')],
             ['source' => __DIR__ . '/../../stubs/approx/resource/assets', 'destination' => public_path('assets')],
         ];
